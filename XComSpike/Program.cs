f8d283e831487d09c8beb24cfe546d2cccde1669
Mike Hall <mike.hall@veteransunited.com>
2023-02-17 14:35:36 -0600

    fix: refactored

--- XComSpike/Program.cs
+++ XComSpike/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Diagnostics;
+﻿using CsvHelper;
+using System;
+using System.Diagnostics;
+using System.Formats.Asn1;
+using System.Globalization;
 using System.Net.Mail;
 using System.Text;
 using XCom.ApiClient.Api;
@@ -14,27 +18,38 @@ namespace XComSpike
 
             var apiInstance = new MailApi("https://xcom.d.vu.local/v2");
             var application = ApplicationType.SsOps;
-            var templateId = "d-72603a604ca64fcb8387d260e53eeca0";
+            var templateId = "d-3a1fdae1e49c417f8a31bbc9f3d81d4d";
             var fields = new Dictionary<string, string>();
-            fields.Add("4000", "Test");
-            fields.Add("4002", "Testing");
-            DynamicTemplateData templateData = new DynamicTemplateData {{ "4000", "Test" } };
+            DynamicTemplateData templateData = new DynamicTemplateData { { "TotalEmails", "420" }, { "NeedsCall", "69" } };
             var toEmail = "mike.hall@veteransunited.com";
+            var toEmail2 = "ross.henke@veteransunited.com";
             var fromEmail = "no_reply@vu.com";
 
+
+            var needsCalledResults = new List<Loan> { new Loan(1, "1"), new Loan(2, "2"), new Loan(3, "3") };
+
             //Attempting to attach a text file to email
-            var fileName = "TextFile1.txt";      
+            var fileName = "FundingOverdueList.csv";
+            var myFile = File.Create(fileName);
+            myFile.Close();
+
+            using (var writer = new StreamWriter(fileName))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(needsCalledResults.Select(loan => loan.LoanNumber).ToList());
+            }
             var fileContents = File.ReadAllText(fileName);
-            var bytes = Encoding.UTF8.GetBytes(fileContents);
-            var b = Convert.ToBase64String(bytes);
-            var fileToAttach = new XCom.ApiClient.Model.Attachment(b, "txt", fileName, "test", "test");
+
+            var overdueFundingListByteArray = Encoding.UTF8.GetBytes(fileContents);
+            var overdueFundingListBase64String = Convert.ToBase64String(overdueFundingListByteArray);
+            var fileToAttach = new XCom.ApiClient.Model.Attachment(overdueFundingListBase64String, "txt", fileName, "test", "test");
             var attachments = new List<XCom.ApiClient.Model.Attachment> { fileToAttach };
 
             var mailRequest = new MailRequest(
                 application: application,
                 templateId: templateId,
                 fromEmail: fromEmail,
-                recipients: new Recipients(to: new List<string> { toEmail }),
+                recipients: new Recipients(to: new List<string> { toEmail, toEmail2 }),
                 dynamicTemplateData: templateData,
                 attachments: attachments,
                 sendTestEmail: 1);
@@ -49,6 +64,18 @@ namespace XComSpike
             {
                 Debug.Print("Exception when calling MailApi.SendMail: " + e.Message);
                 Console.WriteLine("Exception when calling MailApi.SendMail: " + e.Message);
+            }
+
+        }
+
+        public class Loan
+        {
+            public int LoanNumber { get; set; }
+            public string Name { get; set; }
+            public Loan(int loanNumber, string name)
+            {
+                LoanNumber = loanNumber;
+                Name = name;
             }
         }
     }
